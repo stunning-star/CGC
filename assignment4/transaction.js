f3e110344342f166d7bb014021b9ff944cf3af82
stunning-star <mohdsafiuddin99@gmail.com>
2025-02-20 14:30:44 +0530

    jsupdated constructor

--- assignment4/transaction.js
+++ assignment4/transaction.js
@@ -3,7 +3,10 @@ withdrawal=500;
 function checkBalance(availableBalance,withdrawal){
 if (withdrawal>availableBalance){
     console.log("Insufficient funds");
-} else {
+} else if (withdrawal%100!=0||withdrawal%500!=0 || withdrawal%200!=0 || withdrawal%2000!=0){
+    console.log("Invalid amount");
+}
+else {
     availableBalance=availableBalance-withdrawal;
     console.log("Withdrawal successful. Available balance is:",availableBalance);
 }
